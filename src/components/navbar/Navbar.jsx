bc0df7613dd51d45bc5ceeb8f889248273e69b1a
Anushka Singh <anushkasinghas1512@gmail.com>
2025-05-24 16:33:56 +0530

    changes name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import "./navbar.scss"
-import Sidebar from "../sidebar/Sidebar"
+import Sidebar from "../sidebar/Sidebars"
 import {motion} from "framer-motion"
 
 const Navbar = () => {
